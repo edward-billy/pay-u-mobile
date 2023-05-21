fa2304ff0d9e9b229dd5e516a803e129375bca95
edward-billy <edwardbilly.h@gmail.com>
2023-05-21 17:25:48 +0700

    added historyDetail and minor update in history

--- lib/views/history.dart
+++ lib/views/history.dart
@@ -2,9 +2,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:payu/app/api.dart';
-import 'buttonnav.dart';
-import 'package:number_pagination/number_pagination.dart';
-import 'home.dart';
+import 'package:intl/intl.dart';
+import 'historyDetail.dart';
 import 'login.dart';
 
 class HistoryScreen extends StatefulWidget {
@@ -14,14 +13,6 @@ class HistoryScreen extends StatefulWidget {
 
 class _HistoryScreenState extends State<HistoryScreen> {
   List<Map<String, dynamic>> historyData = [];
-  int currentPage = 1;
-  int totalPages = 1;
-
-  void setPage(int page) {
-    setState(() {
-      currentPage = page;
-    });
-  }
 
   @override
   void initState() {
@@ -29,16 +20,10 @@ class _HistoryScreenState extends State<HistoryScreen> {
     loadHistoryData();
   }
 
-  int calculateTotalPages(int totalItems) {
-    const double pageSize = 8;
-    return (totalItems / pageSize).ceil();
-  }
-
   Future<void> loadHistoryData() async {
     List<Map<String, dynamic>> data = await getHistoryData();
     setState(() {
       historyData = data;
-      totalPages = calculateTotalPages(data.length);
     });
   }
 
@@ -66,12 +51,27 @@ class _HistoryScreenState extends State<HistoryScreen> {
                   DataCell(Text(historyData[index]['invoiceId'].toString())),
                   DataCell(Text(historyData[index]['name'].toString())),
                   DataCell(Text(historyData[index]['nama'].toString())),
-                  DataCell(Text('Rp. ${historyData[index]['total']}-')),
+                  DataCell(Text(
+                    NumberFormat.currency(
+                      locale: 'id',
+                      symbol: 'Rp ',
+                    ).format(historyData[index]['total']),
+                  )),
                   DataCell(
                     ElevatedButton(
                       onPressed: () {
-                        // Handle button press
-                        // You can navigate to the detail screen here
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HistoryDetail(
+                              invoiceId:
+                                  historyData[index]['invoiceId'].toString(),
+                              data: getHistoryDetail((index + 1)),
+                              nama: historyData[index]['nama'].toString(),
+                              name: historyData[index]['name'].toString(),
+                            ),
+                          ),
+                        );
                       },
                       child: const Text('Detail'),
                     ),
@@ -97,9 +97,10 @@ class _HistoryScreenState extends State<HistoryScreen> {
 
       localStorage.remove('user');
       localStorage.remove('token');
-      // ignore: use_build_context_synchronously
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => const Login()));
+        context,
+        MaterialPageRoute(builder: (context) => const Login()),
+      );
     }
   }
 }
@@ -107,9 +108,17 @@ class _HistoryScreenState extends State<HistoryScreen> {
 Future<List<Map<String, dynamic>>> getHistoryData() async {
   var res = await Network().getData('/history');
   var body = json.decode(res.body);
-  // print(body['data']['data']);
   if (body['data'] != null) {
     return List<Map<String, dynamic>>.from(body['data']['data']);
   }
-  return []; // Return an empty list if there's an error or no data
+  return [];
+}
+
+Future<List> getHistoryDetail(id) async {
+  var res = await Network().getData('/history/$id');
+  var body = json.decode(res.body);
+  if (body['data'] != null) {
+    return List<dynamic>.from(body['data']);
+  }
+  return [];
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -69,6 +69,7 @@ class _HomeState extends State<Home> {
           DashboardScreen(),
           ProdukScreen(),
           HistoryScreen(),
+          HistoryScreen(),
         ],
       ),
       bottomNavigationBar: BottomNav(
